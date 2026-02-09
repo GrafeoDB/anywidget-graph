ba190e4a32cd4c47b30347852cd5211ba126809c
StevenBtw <stevengrond@gmail.com>
2026-02-09 21:50:57 +0100

    fix (hopefully?)

--- src/anywidget_graph/ui/__init__.py
+++ src/anywidget_graph/ui/__init__.py
@@ -93,7 +93,7 @@ import neo4j from "https://cdn.jsdelivr.net/npm/neo4j-driver@5.28.0/lib/browser/
 // === Main Entry ===
 {_resolve_neo4j_namespace(index_js)}
 
-export {{ render }};
+export default {{ render }};
 """
 
 
